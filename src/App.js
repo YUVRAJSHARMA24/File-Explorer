601051b72627dc5f8ea4145a6c06ebb2dfa57269
YUVRAJSHARMA24 <yuvraj.june2002@gmail.com>
2024-03-10 10:54:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-
-
+import { useState } from "react";
+import explorer from "./data/folderData";
+import Folder from "./components/Folder";
 function App() {
+  const [explored, setExplored] = useState(explorer);
   return (
     <>
+      <Folder explorer={explored} />
     </>
   );
 }
